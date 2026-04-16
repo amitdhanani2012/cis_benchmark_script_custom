6b77eb1a9d5ac2645d7b33cc153fde7c8e67fd8e
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-16 19:57:01 +0530

    second commit improve gid group exist or not

--- passwd_group_in_group_file.sh
+++ passwd_group_in_group_file.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+declare -A gid
+x=0
 for i in `awk -F':' '{ print $4 }' /etc/passwd`
 do
 #echo $i
@@ -7,8 +9,18 @@ grep $i  /etc/group >/dev/null
 if [ $? -eq 0 ];then
   echo "$i group id exist"
 else
-  echo "$i group does not exist" 
+  #echo "$i group does not exist" 
+  mark="True" 
+  gid[$x]=$i
+  x=`expr $x + 1`
+
 fi
 
 done
 
+if [ $mark == "True" ];then
+for i in ${gid[@]}
+do
+echo "Amit: group $i does not exist"
+done
+fi
