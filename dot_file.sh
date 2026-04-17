702b1e09645eb48fb58cde89cc80f3b6ec7afc57
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-17 14:03:58 +0530

    dot file and log file fix

--- dot_file.sh
+++ dot_file.sh
@@ -5,5 +5,5 @@ home1_path=$(awk -F':' '{ if ($3>999 && $7 !~ /nologin/) print $6}' /etc/passwd)
 for home_path in `echo $home1_path`
 do
 
-find $home_path -type f -iname ".*" \( -perm 777 -o -perm 644 -o -perm 666  \) -exec chmod 640 {} \;
+find $home_path -type f -iname ".*" \( -perm 777 -o -perm 644 -o -perm 666 -o -perm 660  \) -exec chmod 640 {} \;
 done
---
+++ log_file_perm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i 's/^create.*/create 640/g' /etc/logrotate.conf
+sapath=$(rpm -ql sysstat |grep -e "sa1$" -e "sa2$")
+
+if [ -n "$sapath" ];then
+for i in `echo $sapath`
+do
+sed -i 's/umask.*/umask 027/g' $i
+done
+fi
+
+find /var/log/ -type f \( -perm 666 -o -perm 664 -o -perm 644 -o -perm 660 \) -exec chmod 640 {} \;
+find /var/log/ -type d \( -perm 777 -o -perm 755 -o -perm 770 -o perm 775 \)-exec chmod 750 {} \;
+
