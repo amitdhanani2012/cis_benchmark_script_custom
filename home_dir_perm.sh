692c646f3fe25f7b446043039cfb8af0c09dad5d
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-18 16:46:46 +0530

    home dir group fix

--- home_dir_perm.sh
+++ home_dir_perm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-home1_path=$(awk -F':' '{ if ($3>999 && $7 !~ /nologin/) print $1"-"$5"-"$6}' /etc/passwd)
+home1_path=$(awk -F':' '{ if ($3>999 && $7 !~ /nologin/) print $1"-"$4"-"$6}' /etc/passwd)
 
 echo $home1_path
 
@@ -10,11 +10,13 @@ do
 user=$(echo $home_path |cut -f1 -d'-')
 group=$(echo $home_path|cut -f2 -d'-')
 path1=$(echo $home_path|cut -f3 -d'-')
-echo $user
-echo $group
-echo $path1
+#echo $user
+#echo $group
+#echo $path1
+grep $group /etc/group >/dev/null && group1=$(grep $group /etc/group|cut -f1 -d':')
+#echo "group is $group1"
 ls -ld $path1 |grep -e "drwxrwxrwx"  -e "drwxr-xr-x" -e "drwxrwx---" && chmod 750 $path1 
-if [ -n "$user" -a -n "$group" ];then
+if [ -n "$user" -a -n "group1" ];then
 chown $user.$group $path1
 fi
 done
