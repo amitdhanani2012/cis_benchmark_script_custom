102fd622ced1dca2bee0cbcbdeae210c1988a013
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-18 16:18:54 +0530

    fixed home_dir_perm.sh

--- home_dir_perm.sh
+++ home_dir_perm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-home1_path=$(awk -F':' '{ if ($3>999 && $7 !~ /nologin/) print $6}' /etc/passwd)
+home1_path=$(awk -F':' '{ if ($3>999 && $7 !~ /nologin/) print $1"-"$5"-"$6}' /etc/passwd)
+
+echo $home1_path
+
 
 for home_path in `echo $home1_path`
 do
-
-ls -ld $home_path |grep -e "drwxrwxrwx"  -e "drwxr-xr-x" -e "drwxrwx---" && chmod 750 $home_path
+user=$(echo $home_path |cut -f1 -d'-')
+group=$(echo $home_path|cut -f2 -d'-')
+path1=$(echo $home_path|cut -f3 -d'-')
+ls -ld $path1 |grep -e "drwxrwxrwx"  -e "drwxr-xr-x" -e "drwxrwx---" && chmod 750 $home_path 
+chown $user.$group $path1
 done
