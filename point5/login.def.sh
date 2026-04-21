3e1d5d9837de7696480be5158128035c6fcd9617
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-21 13:54:05 +0530

    login.def fix

--- point5/login.def.sh
+++ point5/login.def.sh
@@ -2,6 +2,8 @@
 #set -x
 u1=$(grep "^UMASK" /etc/login.defs)
 u2=$(echo $u1 |cut -f2 -d' ' )
-if [ "$u2" != "027" ];then
+if [ "$u2" != "027" -a -n "$u2" ];then
 sed -i 's/UMASK.*/UMASK 027/' /etc/login.defs
+elif [ -z "$u2" ];then
+echo "UMASK 027" >>/etc/login.defs
 fi
