1f827e6722332799af375a965beb3de7b93a0ccb
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-21 17:33:38 +0530

    non-interactive-shell

--- non-interactive-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-#user=$(awk -F':' '{ if ($3<=999 && $7 !~ /nologin/ && $4 != 0 ) print $0 }' /etc/passwd)
-user1=$(awk -F':' '{ if ($3<=999 && $7 !~ /nologin/ && $3 != 0 && $7 == "/bin/bash" ) print $1 }' /etc/passwd)
-if [ -n "$user1" ];then
-grep -q $user1 /etc/passwd && sed -i  "/${user1}/s/\/bin\/bash/\/usr\/bin\/nologin/" /etc/passwd
-fi
