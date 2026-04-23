71457af944aff4e365c0019e737653d780ac02c0
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-23 22:21:05 +0530

    aid 6.1.3 fix aide init

--- point6_all/aide_6_1_3.aide.conf.sh
+++ point6_all/aide_6_1_3.aide.conf.sh
@@ -1,2 +1,4 @@
 printf '\n%s' "# Audit Tools" "$(readlink -f /sbin/auditctl) p+i+n+u+g+s+b+acl+xattrs+sha512" "$(readlink -f /sbin/auditd) p+i+n+u+g+s+b+acl+xattrs+sha512" "$(readlink -f /sbin/ausearch) p+i+n+u+g+s+b+acl+xattrs+sha512" "$(readlink -f /sbin/aureport) p+i+n+u+g+s+b+acl+xattrs+sha512" "$(readlink -f /sbin/autrace) p+i+n+u+g+s+b+acl+xattrs+sha512" "$(readlink -f /sbin/augenrules) p+i+n+u+g+s+b+acl+xattrs+sha512" >> /etc/aide.conf
+aide --init >/dev/null
+mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
 echo "done with auditctl"
