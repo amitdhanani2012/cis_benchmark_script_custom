891c926f1f7b596793276798a9e87a62fc56abe7
Amit Kamalbhai Dhanani <amitdhanani2012@gmail.com>
2026-04-23 16:36:44 +0530

    rsyslog

--- point6_all/rsyslog_filecreate.sh
+++ point6_all/rsyslog_filecreate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
 (grep "^\$FileCreateMode" /etc/rsyslog.conf && sed -i "s/\$FileCreateMode.*/\$FileCreateMode 0640/" /etc/rsyslog.conf)>/dev/null || echo "\$FileCreateMode 0640" >> /etc/rsyslog.conf
-echo "Starting validation"
-rsyslogd -N1
+#echo "Starting validation"
+systemctl restart rsyslog 
+if [ $? -eq 0 ];then
 echo "done with rsyslog file create mode 640"
+fi
